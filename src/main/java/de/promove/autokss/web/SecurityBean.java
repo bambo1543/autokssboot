1d92a49ef9a89ac3a379cc5a2ce3b779617dbb45
bambo1543 <andreas@bambo.it>
2022-04-25 14:18:35 +0200

    release 0.0.1

--- src/main/java/de/promove/autokss/web/SecurityBean.java
+++ src/main/java/de/promove/autokss/web/SecurityBean.java
@@ -1,5 +1,8 @@
 package de.promove.autokss.web;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -13,9 +16,14 @@ public class SecurityBean implements Serializable {
 
     private static final long serialVersionUID = 1234568L;
 
+    @Getter
+    @Autowired
+    private BuildProperties buildProperties;
+
     public boolean isLoggedIn() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return !"anonymousUser".equals(authentication.getPrincipal());
     }
+
 
 }
