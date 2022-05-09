474ca88749f3e053a140750ea27adf3ceb07399d
bambo1543 <andreas@bambo.it>
2022-05-09 09:02:07 +0200

    jndi support for flyway

--- src/main/java/de/promove/autokss/configuration/FlywayJndiConfiguration.java
+++ src/main/java/de/promove/autokss/configuration/FlywayJndiConfiguration.java
@@ -3,12 +3,14 @@ package de.promove.autokss.configuration;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jndi.JndiTemplate;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 @Configuration
+@Profile({"mariadb, jndi-mariadb"})
 public class FlywayJndiConfiguration {
 
     @Bean
